01709011a8892ed56b59deab170ed9c76430135d
David Edell <david.edell@jhuapl.edu>
2023-07-07 14:10:55 -0400

    Updated to use latest version of Python CFDP library and added additional configuration options.

--- cfdp_wrapper/cfdp_wrapper/cfdp_wrapper.py
+++ cfdp_wrapper/cfdp_wrapper/cfdp_wrapper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import rclpy
 from rclpy.node import Node
@@ -10,31 +11,68 @@ from rcl_interfaces.msg import ParameterDescriptor
 
 import cfdp
 import struct
-from cfdp.transport import Transport
+from cfdp.transport.base import Transport
 from cfdp.filestore import NativeFileStore
 from cfdp.constants import ConditionCode, FaultHandlerAction
 
 from cfdp_msgs.srv import CfdpCmd, CfdpXfrCmd
-from cfdp_msgs.msg import CfdpPdu
+#from cfdp_msgs.msg import CfdpPdu
+from cfe_msgs.msg import BinaryPktPayload  # Default binary packet format
+
+DEFAULT_MAXIMUM_PACKET_LENGTH = 4096
 
 class RosTransport(Transport):
-    def __init__(self):
+    
+    # entities provides Entity definitions and routing information
+    def __init__(self, entities, maximum_packet_length=DEFAULT_MAXIMUM_PACKET_LENGTH):
         super().__init__()
         self.sent = 0
+        self.entities = entities
+        self.maximum_packet_length = maximum_packet_length
+
+        # From newer transports
+        # self._thread = threading.Thread(target=self._incoming_pdu_handler)
+        # self._thread.kill = False
         
     def bind(self):
         print("DEBUG: RosTransport.bind")
-        # Do we need to do anything here?
+        # Do we need to do anything here? Handled extenally for Ros Transport
+
+    def unbind(self):
+        printf("DEBUG: RosTransport.unbind")
+        # Nothing to be done
         
-    def request(self, data, address):
-        print(f"CFDP Sending: len={len(data)}, Typeof {type(data)}, address= {type(address)}, {address}")
+    def request(self, data):        
+        # address parameter is missing from cfdp v2.0.0 in favror of routing as parameter, but entity id is missing as parameter
+        entity = self._get_entity_from_data(data)
+        if not entity or not 'publisher' in entity:
+            print("CFDP ERROR: Cannot send to invalid entity")
+            return
+        
+        print(f"CFDP Sending: len={len(data)}, Typeof {type(data)}, entity= {entity}")
 
         # ROS doesn't seem to allow a msg definition of a simple bytes array, so we must split it
         #  into an array of individual byte objects (byte[])
         msg_data = [i.to_bytes(1, sys.byteorder) for i in data]
-        address['publisher'].publish(CfdpPdu(data=msg_data))
+
+        print(f"publish msg.data=" + str(msg_data))
+        
+        entity['publisher'].publish(BinaryPktPayload(data=msg_data))
         self.sent = self.sent+1
 
+    def _get_entity_from_data(self, data):
+        pdu = cfdp.PduHeader.decode(data)
+        #print(pdu, pdu.pdu_type, pdu.direction, pdu.source_entity_id, pdu.destination_entity_id, pdu.transaction_seq_number)
+
+        if pdu.direction == 1:
+            entity_id = pdu.source_entity_id
+        else:
+            entity_id = pdu.destination_entity_id
+            
+        #print(f"data has entity_id={entity_id}")
+        return self.entities[entity_id]
+        
+        
 class CFDPWrapper(Node):
 
     def __init__(self):
@@ -51,50 +89,64 @@ class CFDPWrapper(Node):
 
         filestoreDescriptor = ParameterDescriptor(description='The base path (relative to script launch location) for all local file operations')
         self.declare_parameter("filestore", "cfdp/rosfsw", filestoreDescriptor)
-        fileStore = self.get_parameter("filestore").get_parameter_value().string_value
+        self.fileStore = self.get_parameter("filestore").get_parameter_value().string_value
 
         altServicesDesc = ParameterDescriptor(description="If true, append '/$entityID' to all generated services to enable testing with multiple instances")
         self.declare_parameter('altServices', False)
         useAltServices = self.get_parameter("altServices").get_parameter_value().bool_value
 
+        pduPrefixDesc = ParameterDescriptor(description="Define topic prefix (with entityID appended) for all PDU subscriptions and publications.")
+        self.declare_parameter('pduTopicPrefix', "/cfdp/pdu/entity")
+        pduTopicPrefix = self.get_parameter("pduTopicPrefix").get_parameter_value().string_value
+
+        
         cfgDescriptor = ParameterDescriptor(description='TODO: Load additional configuration parameters from file')
         self.declare_parameter("config", "src/cfe_cfdp/cfdp_wrapper/cfdp_wrapper/config/cfdp_config.yaml")
         cfg_file = self.get_parameter("config").get_parameter_value().string_value
         
         # TODO: Load entities from cfg_file (or directly from ROS parameters if possible)
-        self.entities = [
-            {
+        # Note: key is entity ID and should match id attribute (if not defined, it will be set automatically bleow)
+        self.entities = {
+            1: {
                 "name": "GSW-ROS",
                 "id": 1,
                 "apid": 0x08C2
             },
-            {
+            2: {
                 "name": "FSW-ROS",
                 "id": 2,
                 "apid": 0x08C3
             },
-            {
+            25: {
                 "name": "cFE",
                 "id": 25,
                 "apid": 0x18c8
             }
-        ]
+        }
+ 
+        local_cnt = 0
+        remote_cnt = 0
+        for entityid in self.entities:
+            # Cleanup and sanity checks
+            entity = self.entities[entityid]
+            if 'id' not in entity:
+                entity['id'] = entityid
 
-        remote_entities=[]
-        local_entity = None
-        for entity in self.entities:
+            # Setup publisher/receiver details
             if entity['id'] == self.entityID:
-                local_entity = cfdp.LocalEntity(entity['id'], entity['apid'])
+                local_cnt += 1
             else:
                 entity['publisher'] = self.create_publisher(
-                    CfdpPdu, # ByteMultiArray,  # Bytes or byteMultiArray?
-                    '/cfdp/pdu/entity' + str(entity['id']),
+                    BinaryPktPayload, # ByteMultiArray,  # Bytes or byteMultiArray?
+                    pduTopicPrefix + str(entity['id']),
                     10
                 )
-                remote_entities.append(cfdp.RemoteEntity(entity['id'], entity))
+                remote_cnt += 1
+                #remote_entities.append(cfdp.RemoteEntity(entity['id'], entity))
 
 
-        if not local_entity or not remote_entities:
+        if local_cnt!=1 or remote_cnt==0:
+            print(f" local_cnt={local_cnt} and remote_cnt={remote_cnt}")
             raise ValueError("Local and Remote Entities must be defined to initialize this application")
         else:
             self.get_logger().warn(f"entityID={self.entityID}, cfg={cfg_file}entities= {self.entities}")
@@ -106,18 +158,17 @@ class CFDPWrapper(Node):
         self._timer = self.create_timer(self._timer_period, self.timer_callback)
         
         # Setup CFDP Entity, Transport Service, and ROS Publisher interface
-        self.cfdp_ts = RosTransport()
-        config = cfdp.Config(
-            local_entity = local_entity,
-            remote_entities=remote_entities,
-            filestore=NativeFileStore(fileStore), # path is relative to script launch dir
+        self.cfdp_ts = RosTransport(self.entities)
+        self.cfdp = cfdp.CfdpEntity(
+            entity_id=self.entityID,
+            filestore=NativeFileStore(self.fileStore), # path is relative to script launch dir
             transport=self.cfdp_ts
             )
-        self.cfdp = cfdp.CfdpEntity(config)
+
 
         # Create ROS subscription for inbound MDPUs
-        self._subscribe_pdu = self.create_subscription(CfdpPdu,
-                                                        '/cfdp/pdu/entity' + str(self.entityID),
+        self._subscribe_pdu = self.create_subscription(BinaryPktPayload,
+                                                        pduTopicPrefix + str(self.entityID), # DBG: 25', # RESTORE THIS
                                                         self.cfdp_handle_packet, 10)
         
         ### Create Services for Local/User Commanding
@@ -130,7 +181,7 @@ class CFDPWrapper(Node):
                                                                    self.cfdp_cmd_put)
         self._trigger_cfdp_cmd_get_srv = self.create_service(CfdpXfrCmd,
                                                              '/cfdp/cmd/get' + servicePrefix,
-                                                                   self.cfdp_cmd_put)
+                                                                   self.cfdp_cmd_get)
 
 
     def timer_callback(self):
@@ -153,19 +204,28 @@ class CFDPWrapper(Node):
         dstid = request.dstid
         srcfile = request.src # "/test.txt"
         dstfile = request.dst # "/cf/testdst.txt"
-        self.get_logger().info("Issuing CFDP Put " + srcfile + " to " + dstfile)
+        self.get_logger().info(f"Issuing CFDP Put {srcfile} to {dstfile} on {dstid}")
+
+        # Verify src file exists locally
+        if not os.path.isfile(os.path.join(self.fileStore, srcfile)):
+            self.get_logger().warn(f"Can't execute CFDP Put command for non-existent file {srcfile}")
+            return response
+        
+        if not self.entities[dstid]:
+            self.get_logger().warn(f"Can't executed CFDP Command to unknown dstid={dstid}")
+            return response # TODO: Is there a canonical way to signal to ROS that cmd failed?
 
         try:
             transaction_id = self.cfdp.put(
                 destination_id=dstid, #0x19,
                 source_filename=srcfile,
                 destination_filename=dstfile,
-                #transmission_mode=cfdp.TransmissionMode.UNACKNOWLEDGED
-                transmission_mode=cfdp.TransmissionMode.ACKNOWLEDGED,
+                transmission_mode=cfdp.TransmissionMode.UNACKNOWLEDGED,  # TODO: Make this a flag
+                #transmission_mode=cfdp.TransmissionMode.ACKNOWLEDGED, # FIXME: Bug. In this mode, reply is seemingly sent to wrong dst_id. Unless ACKs reverse header fields?
                 fault_handler_overrides={
                     ConditionCode.POSITIVE_ACK_LIMIT_REACHED: FaultHandlerAction.ABANDON}            
             )
-        except Exception as err:
+        except BasseException as err:
             self.get_logger().error(f"Unable to execute PUT command for: {request}.  Err Type {type(err)}, Err: {err}")
         
         return response
@@ -173,10 +233,15 @@ class CFDPWrapper(Node):
     def cfdp_cmd_get(self, request, response):
         self.get_logger().info("Issuing CFDP Get " + request.src + " to " + request.dst)
 
+        if not os.path.isdir( os.path.join( self.filestore, os.path.dirname(request.dst) ) ):
+            self.get_logger().info(f"Can't execute CFDP Get Command to invalid destination path {request.dst}.")
+            return response
+        
         try:
             transaction_id = self.cfdp.put(
                 destination_id=request.dstid,
                 transmission_mode=cfdp.TransmissionMode.ACKNOWLEDGED,
+
                 messages_to_user=[
                     # We PUT a request to PUT a file back to us
                     cfdp.ProxyPutRequest(
@@ -192,14 +257,16 @@ class CFDPWrapper(Node):
 
     # Handle receipt of ROS Topic containing Binary MDPU as it's content
     def cfdp_handle_packet(self, msg):
-        self.get_logger().warn(f"CFDP received PDU {type(msg)} of length {len(msg.data)}")
+        try:
+            self.get_logger().warn(f"CFDP received PDU {type(msg)} of length {len(msg.data)}")
 
-        # Python + ROS usage of byte definitions are inconsistent, so we need to
-        # explicitly rejoin an array of individual bytes into a bytes object
-        bs = b''.join(msg.data)
+            # Python + ROS usage of byte definitions are inconsistent, so we need to
+            # explicitly rejoin an array of individual bytes into a bytes object
+            bs = b''.join(msg.data)
         
-        self.cfdp.transport.indication(bs)
-
+            self.cfdp.transport.indication(bs)
+        except FileNotFoundError:
+            self.get_logger().warn(f"CFDP File Error: {str(e)}")
 
 
 def main(args=None):
